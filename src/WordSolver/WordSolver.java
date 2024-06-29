c1a766875088010f4a7a9d573825d09624b2ff13
jullouie <jljz2022@mymail.pomona.edu>
2024-06-29 19:21:25 -0400

    can print the words based on prefix or word length

--- src/WordSolver/WordSolver.java
+++ src/WordSolver/WordSolver.java
@@ -2,7 +2,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 import java.util.Set;
 
 public class WordSolver {
@@ -10,7 +15,13 @@ public class WordSolver {
     private char[][] board = new char[4][4];
     private Trie dictionary;
     private Set<String> wordsFound;
+    private HashMap<String, PriorityQueue<String>> prefixes;
+    private HashMap<String, HashSet<String>> prefixWordSets;
+    private PriorityQueue<String> orderedWords;
     private boolean[][] visited;
+    private Comparator<String> greatestToL;
+    private Comparator<String> leastToG;
+    private String prefix = "";
 
     // to find the perimeter of 8 letters around (x,y)
     private static final int[] xpos = {-1, -1, -1, 0, 0, 1, 1, 1};
@@ -24,6 +35,11 @@ public class WordSolver {
         this.dictionary = new Trie();
         this.wordsFound = new HashSet<>();
         this.visited = new boolean[4][4];
+        this.greatestToL = (a,b) -> Integer.compare(b.length(), a.length());
+        this.leastToG = (a,b) -> Integer.compare(a.length(), b.length());
+        this.orderedWords = new PriorityQueue<String>(greatestToL);
+        this.prefixes = new HashMap<String, PriorityQueue<String>>();
+        this.prefixWordSets = new HashMap<String, HashSet<String>>();
     }
 
     /**
@@ -38,6 +54,16 @@ public class WordSolver {
         }
         reader.close();
         // System.out.println("dictionary done");
+    }
+
+    /**
+     * @return a stack in order of longest word to shortest
+     */
+    public PriorityQueue<String> getOrderedWords(){
+        for(String word : wordsFound){
+            orderedWords.add(word);
+        }
+        return orderedWords;
     }
 
     /**
@@ -76,9 +102,19 @@ public class WordSolver {
             return;
         }
 
+
         // search for current word in dictionary
-        if (currentWord.length() >= n && dictionary.contains(currentWord)){
+        if (currentWord.length() >= n && dictionary.contains(currentWord)) {
             wordsFound.add(currentWord);
+            if (currentWord.length() >= 3) {
+                prefix = currentWord.substring(0, 2);
+                prefixWordSets.putIfAbsent(prefix, new HashSet<String>());
+                if (!prefixWordSets.get(prefix).contains(currentWord)) {
+                    prefixWordSets.get(prefix).add(currentWord);
+                    prefixes.putIfAbsent(prefix, new PriorityQueue<>(greatestToL));
+                    prefixes.get(prefix).add(currentWord);
+                }
+            }
         }
 
         visited[x][y] = true;
@@ -92,14 +128,33 @@ public class WordSolver {
         visited[x][y] = false;
     }
 
+    public  void leastToString(){
+        for (PriorityQueue<String> valueQueue : prefixes.values()) {
+            System.out.println(valueQueue);
+        }
+    }
+
     public static void main(String[] args) throws IOException{
-        WordSolver solver = new WordSolver("cttrsoebfqscaodw");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Do you want the words to be grouped by prefix?");
+        String ans = userInput.nextLine();
+        System.out.println("Enter the 16 letters:");
+        String str = userInput.nextLine();
+        userInput.close();
+        WordSolver solver = new WordSolver(str);
         //add in a filename
         solver.createDictionary("src/WordSolver/words.txt");
         Set<String> words = solver.findWords(5);
-        
-        for (String word : words) {
-            System.out.println(word);
+        if (ans.equals("yes") || ans.equals("y")){
+            solver.leastToString();
+        } else {
+            for (String word : words) {
+                System.out.println(word);
+            }
         }
+
+        // store foundWords in a stack with own comparator where compares length of word
+        // based on prefixes - give all of them for each three letter prefix
+        // make a hashmap of {prefix, list of Strings} then put in a stack ordered based on longest list of strings
     }
 }
